96a985d3dce2e271cd3b45d1ba0ace9f7a9b3e9d
larsbieber <lbieber1@smail.uni-koeln.de>
2023-07-01 14:48:03 +0200

    Task 1 done

--- src/test/java/idh/java/TestATM.java
+++ src/test/java/idh/java/TestATM.java
@@ -11,8 +11,35 @@ import idh.java.ATM.IllegalInputException;
 
 public class TestATM {
 	
+	ATM myATM;
 	
+	@BeforeEach
+	void setup() {
+		myATM = new ATM();
+	}
 	
-	
+	@Test
+	void testBills() throws IllegalInputException {
+		assertArrayEquals(new int[] {0,0,0,0,0,0,1}, myATM.convertToBills(5));
+		assertArrayEquals(new int[] {0,0,0,0,0,1,0}, myATM.convertToBills(10));
+		assertArrayEquals(new int[] {0,0,0,0,1,0,0}, myATM.convertToBills(20));
+		assertArrayEquals(new int[] {0,0,0,1,0,0,0}, myATM.convertToBills(50));
+		assertArrayEquals(new int[] {0,0,1,0,0,0,0}, myATM.convertToBills(100));
+		assertArrayEquals(new int[] {0,1,0,0,0,0,0}, myATM.convertToBills(200));
+		assertArrayEquals(new int[] {1,0,0,0,0,0,0}, myATM.convertToBills(500));
+		assertArrayEquals(new int[] {1,0,0,0,0,0,1}, myATM.convertToBills(505));
+		assertArrayEquals(new int[] {0,0,0,0,1,1,0}, myATM.convertToBills(30));
+		assertArrayEquals(new int[] {1,1,1,1,1,1,1}, myATM.convertToBills(885));
+		assertArrayEquals(new int[] {0,0,0,1,0,1,1}, myATM.convertToBills(65));
+	}
 
+	@Test
+	void testNegative() throws IllegalInputException {
+		assertArrayEquals(new int[] {0,0,0,0,0,0,0}, myATM.convertToBills(-5));
+	}
+	
+	@Test
+	void testNotByFive() throws IllegalInputException {
+		assertArrayEquals(new int[] {0,0,0,0,0,0,1}, myATM.convertToBills(7));
+	}
 }
