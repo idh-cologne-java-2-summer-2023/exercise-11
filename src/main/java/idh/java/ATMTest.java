13ae7b088a394aeaccf47330c3b0c7d6b486207f
jelde <jelde@172.29.221.189>
2023-07-03 13:31:13 +0200

    Aufgabe 1 erledigt

---
+++ src/main/java/idh/java/ATMTest.java
@@ -0,0 +1,29 @@
+package idh.java;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import idh.java.ATM.IllegalInputException;
+
+public class ATMTest {
+	
+	@Test
+	public void testConvertToBills() throws IllegalInputException {
+		ATM atm = new ATM();
+		
+		//Betrag ist durch 5 teilbar --> ein 100-Schein erwartet
+		int amount1 = 100;
+		int[] expected1 = new int[] {0, 0, 1, 0, 0, 0, 0};
+		Assertions.assertArrayEquals(expected1, atm.convertToBills(amount1));
+		
+		//Betrag ist nicht durch 5 teilbar --> Exception erwartet
+		int amount2 = 99;
+		Assertions.assertThrows(ATM.IllegalInputException.class, () -> {
+			atm.convertToBills(amount2);
+		});
+		
+		//Betrag ist negativ --> keine Scheine erwartet
+		int amount3 = -100;
+		int[]  expected3 = new int[] {0, 0, 0, 0, 0, 0, 0};
+	}
+}
