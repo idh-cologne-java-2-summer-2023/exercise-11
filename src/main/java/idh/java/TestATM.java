f13f8c4b7708d8655ef45e253d46cd2cb32c1774
laryrauh <laryrauh@aol.com>
2023-07-04 20:57:54 +0200

    Exercise 11 done

---
+++ src/main/java/idh/java/TestATM.java
@@ -0,0 +1,46 @@
+package idh.java;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestATM {
+		
+		private ATM atm;
+		
+		@Before
+		public void setUp() {
+			atm = new ATM();
+		}
+
+		@Test
+		public void testConvertToBills_ValidAmount() throws ATM.IllegalInputException {
+			int amount = 200;
+			int[] expected = {0, 1, 0, 0, 0, 0, 0};
+			int[] result = atm.convertToBills(amount);
+			assertArrayEquals(expected, result);
+		}
+		
+		@Test
+		public void testConvertToBills_NegativeAmount() throws ATM.IllegalInputException {
+			int amount = -100;
+			int[] expected = {0, 0, 0, 0, 0, 0, 0};
+			int[] result = atm.convertToBills(amount);
+			assertArrayEquals(expected, result);
+		}
+		
+		@Test(expected = ATM.IllegalInputException.class)
+		public void testConvertToBills_NonDivisibleAmount() throws ATM.IllegalInputException {
+			int amount = 123;
+			atm.convertToBills(amount);
+		}
+		
+		@Test
+		public void testConvertToBills_LargestAmount() throws ATM.IllegalInputException {
+			int amount = Integer.MAX_VALUE;
+			int[] expected = {2, 3, 1, 1, 1, 0, 1};
+			int[] result = atm.convertToBills(amount);
+			assertArrayEquals(expected, result);
+		}
+	}
