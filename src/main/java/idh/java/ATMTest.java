ce152ea2fea533e2863c75b80e19d49b7a640fe1
Lars <Lars@172.29.252.250>
2023-07-04 19:53:43 +0200

    Init Commit.
    bekomme keine Testergebnisse... Irgendwo liegt wahrscheinlich ein
    Konfigurationsfehler

---
+++ src/main/java/idh/java/ATMTest.java
@@ -0,0 +1,22 @@
+package idh.java;
+//import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+
+public class ATMTest {
+	ATM myTestATM;
+		public static void main(String[] args) {
+			
+		}
+
+		@BeforeEach
+		public void setup() {
+			myTestATM = new ATM();
+		}
+
+
+	
+	
+
+}
