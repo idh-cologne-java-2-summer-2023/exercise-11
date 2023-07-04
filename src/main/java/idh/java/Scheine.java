e80423acbdb5c02debc2b8bcd6cfeb729f37af0d
Carin <Carin@LAPTOP-EO57SIUV.fritz.box>
2023-07-04 22:46:35 +0200

    Updated

--- src/main/java/idh/java/Scheine.java
+++ src/main/java/idh/java/Scheine.java
@@ -1,5 +1,24 @@
 package idh.java;
 
-public class Scheine {
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import idh.java.ATM.IllegalInputException;
+
+
+public class Scheine {
+	
+	public void testConvertToBills_NegativeNumber_ReturnsArrayOfZeros() throws IllegalInputException {
+	    
+		ATM atm = new ATM();
+	    int[] expected = {0, 0, 0, 0, 0, 0, 0}; 
+	    int[] actual = atm.convertToBills(-100);
+	    Assertions.assertArrayEquals(expected, actual);
+	}
+	
+	
+	public void testConvertToBills_PositiveNumberNotDivisibleByFive_ThrowsIllegalInputException() throws IllegalInputException {
+	    ATM atm = new ATM();
+	    atm.convertToBills(123);
+	}
 }
