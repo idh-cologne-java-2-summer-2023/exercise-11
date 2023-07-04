ab7452ee3e67a3472593e79dc071559e69f76ee1
JohannonBlake <johannon.blade@Gmail.com>
2023-07-04 10:36:31 +0200

    test file created and nothing works
    why do these imports never work on my systems?!

---
+++ src/main/java/idh/java/TestATM.java
@@ -0,0 +1,25 @@
+package idh.java;
+//import static org.junit.jupiter.api.Asertions.AssertEquals;
+//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import idh.java.ATM.IllegalInputException;
+public class TestATM {
+
+	ATM testATM = new ATM();
+	public static void main() {
+		
+	
+			
+		}
+
+	@Test
+	void startup() {
+		
+	}
+
+}
