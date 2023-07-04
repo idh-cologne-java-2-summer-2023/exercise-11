8ef920f40ae55a8fc276ede3b69bd763f7a0d5b8
hanna <hanna@172.29.184.160>
2023-07-04 23:14:36 +0200

    Bearbeitet

---
+++ src/main/java/idh/java/Testing.java
@@ -0,0 +1,27 @@
+package idh.java;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * ATM atm;
+
+	
+
+	public void start() {
+		atm = new ATM();
+	}
+
+	
+	public void testFunction() throws IllegalInputException {
+		assertArrayEquals(new int[] {0,1,0,0,0,0,0}, atm.convertToBills(200));
+		assertArrayEquals(new int[] {0,0,2,0,0,0,0}, atm.convertToBills(200));
+		assertArrayEquals(new int[] {1,2,0,0,1,1,0}, atm.convertToBills(430));
+		assertArrayEquals(new int[] {0,0,0,0,0,0,0}, atm.convertToBills(-100));
+		assertThrows(ATM.IllegalInputException.class, () -> {atm.convertToBills(666);});
+
+		 */
+	}
+
+}
