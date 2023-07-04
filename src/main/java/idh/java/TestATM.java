825b4e33558987ff2614f07b12a0063b8d2ffb48
JohannonBlake <johannon.blade@Gmail.com>
2023-07-04 11:03:11 +0200

    imports work
    had to add them to path, but I got the junit imports to work

--- src/main/java/idh/java/TestATM.java
+++ src/main/java/idh/java/TestATM.java
@@ -4,7 +4,7 @@ package idh.java;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import idh.java.ATM.IllegalInputException;
@@ -18,8 +18,18 @@ public class TestATM {
 		}
 
 	@Test
+	@BeforeAll
 	void startup() {
+		ATM testATM = new ATM();
+		assertTrue(cash < 0);
 		
 	}
+	
+	@Test 
+	void billSize() {
+		ATM testATM = new ATM();
+		assertArrayEquals( bills > 0, cashout(amount));
+	}
+	
 
 }
