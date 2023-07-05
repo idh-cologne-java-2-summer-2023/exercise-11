c483800655e992c1c28e104783cf76aae22d9b2e
Jago- <Jago-@192.168.2.163>
2023-07-05 10:59:41 +0200

    task done but : org.opentest4j.AssertionFailedError: array contents differ at index [0], expected: <0> but was: <1>

---
+++ src/main/java/idh/java/UnitTesting.java
@@ -0,0 +1,29 @@
+package idh.java;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import idh.java.ATM.IllegalInputException;
+
+public class UnitTesting {
+        ATM atm;
+
+        @BeforeEach
+        public void startTest() {
+            atm = new ATM();
+        }
+
+        @Test
+        public void testFunction() throws IllegalInputException {
+            assertArrayEquals(new int[] {0,1,0,0,0,0,0}, atm.convertToBills(200));
+            assertArrayEquals(new int[] {0,0,5,0,0,0,0}, atm.convertToBills(500));
+            assertArrayEquals(new int[] {0,2,0,0,2,0}, atm.convertToBills(420));
+            assertArrayEquals(new int[] {0,0,0,0,0,0,0}, atm.convertToBills(-100));
+            assertThrows(ATM.IllegalInputException.class, () -> {atm.convertToBills(666);});
+
+
+        }
+
+}
