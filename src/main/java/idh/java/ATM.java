e2d8b37246838a177cd9e48fa9c48440a08ec98a
Bianca <bianca-00@web.de>
2023-07-12 00:32:04 +0200

    Abgabe 11

--- src/main/java/idh/java/ATM.java
+++ src/main/java/idh/java/ATM.java
@@ -4,21 +4,15 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class ATM  {
+public class BANK  {
 	
-	// initial cash in the ATM
+	
 	int cash = 100;
 		
-	// Which banknotes do we have?
+	
 	int[] value_of_bills = new int[] {500, 200, 100, 50, 20, 10, 5};
 
 	
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -34,20 +28,19 @@ public class ATM  {
 	}
 
 	public void cashout(int amount) {
-		// check for cash in the ATM
+		
 		if (amount > cash) {
 			System.out.println("Sorry, not enough cash left.");
 			return;
 		}
 		
-		// check if value can be divided by 5
+		
 		if (amount % 5 > 0) {
 			System.out.println("Sorry, this amount cannot be expressed in bills.");
 			return;
 		}
 		
 		
-		// withdraw
 		int[] bills = new int[] {0, 0, 0, 0, 0, 0, 0};
 		try {
 			bills = convertToBills(amount);
@@ -70,14 +63,7 @@ public class ATM  {
 		
 	};
 
-	/**
-	 * Converts an amount to an array with the number of bills of each value. 
-	 * This function returns as few bills as possible, i.e., highest value first. 
-	 * (this is not popular in reality ...).
-	 * @param amount
-	 * @return
-	 * @throws IllegalInputException 
-	 */
+
 	protected int[] convertToBills(int amount) throws IllegalInputException {
 		// illegal amount
 		if (amount < 0)
@@ -103,8 +89,8 @@ public class ATM  {
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
-		ATM atm = new ATM();
-		atm.run();
+		BANK bank = new BANK();
+		bank.run();
 	};
 	
 	class IllegalInputException extends Exception {
